ef10db3cb6f950c4d9bf9248e21a5f4523281588
Dmitrij Voznyak <dv98dv@ya.ru>
2023-05-22 20:27:04 +0600

    Задача 2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.Write("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.WriteLine("Большее число: " + a + ", меньшее число: " + b);
+else
+    Console.WriteLine("Большее число: " + b + ", меньшее число: " + a);
